cda700ba08fc803a6acf5b84eec12678b0d46c51
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-06-01 22:08:29 +0530

    Design completed for Wifi Login Component..
    Design completed for Wifi Login Component..

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public myAngularxQrCode: string = null;
   public ssid = 'Harshal';
   public password = 'Raverkar';
   public str: any;
@@ -19,5 +18,13 @@ export class HomeComponent implements OnInit {
 
   ganerateCard() {
     this.str = `WIFI:T:WPA;S:${this.ssid};P:${this.password};`;
+  }
+
+  printQR(divName) {
+    const printContents = document.getElementById(divName).innerHTML;
+    const originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
   }
 }
